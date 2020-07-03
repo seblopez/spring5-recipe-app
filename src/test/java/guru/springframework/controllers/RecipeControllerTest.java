802a1e462a17ee763742582405c48e169f8bcdf0
sebastianlopez <sebalopez2311@gmail.com>
2020-07-03 15:12:54 -0300

    Fixed test

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -114,7 +114,7 @@ public class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/new"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/new"))
+                .andExpect(view().name("recipe/recipeform"))
                 .andExpect(model().attributeExists("recipe"));
 
     }
